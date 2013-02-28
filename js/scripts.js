99e4b00863b8d06c555f81320af85e7c388b9408
Cameron Ketcham <cketcham@gmail.com>
2013-02-28 09:16:39 -0500

    added code to correctly select tab

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,13 @@ $(function() {
  $('#sidebar-call').click(function() {
      $('#call-widget').toggle('slide-down');
  })
+
+ // Automatic tab selection
+ var path = window.location.pathname;
+ path = path.substring(0, path.length - 1);
+ if(path) {
+     $('.nav li').removeClass("active");
+     $('.nav li a[href="'+ path +'"]').parent().addClass("active")
+ }
     
 })
